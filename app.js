37e3d8e07f6f8eb46f578cd12c1a63329cd2bfa7
Harry Baker <harry.baker@ig.com>
2019-07-09 14:18:20 +0100

    added port properly again

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const port = process.env.PORT || 4000
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${process.env.PORT || 3000}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
